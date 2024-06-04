a23b9061da1614bbccdac7ba1573d647e9c791cb
Lalith Sharma <lalithsharma989@gmail.com>
2024-06-04 20:37:05 +0530

    First Commit With Navbar

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,7 @@ import { twMerge } from 'tailwind-merge'
 import { formatDistanceToNowStrict } from 'date-fns'
 import locale from 'date-fns/locale/en-US'
 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
